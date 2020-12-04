fbede68e6d6d71c76067cb20334817c092523658
alekalek <alek@spectra-alarm.com>
2020-12-04 21:39:06 +0100

    Spring Boot backend initialized & added pom.xml file with jaxb dependencies

---
+++ backend/src/main/java/tim1/backend/XmlBackendApplication.java
@@ -0,0 +1,15 @@
+package tim1.backend;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlBackendApplication.class, args);
+
+	}
+
+}
