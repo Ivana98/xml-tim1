dd53694e2e677c802226960ef7919592633aa65c
alekalek <alek@spectra-alarm.com>
2020-12-07 11:48:16 +0100

    hot fix  u zahtev.xml i dodat basic output

--- backend/src/main/java/tim1/backend/model/zahtev/MarshalZahtev.java
+++ backend/src/main/java/tim1/backend/model/zahtev/MarshalZahtev.java
@@ -7,6 +7,8 @@ import javax.xml.bind.Unmarshaller;
 
 public class MarshalZahtev {
     
+    private static String output = "";
+    
     public static void testXmlToObject(){
         try {
             File file = new File("documents/xml_documents/zahtev.xml");
@@ -14,15 +16,36 @@ public class MarshalZahtev {
 
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             Zahtev zahtev = (Zahtev) jaxbUnmarshaller.unmarshal(file);
+
+            //formatiranje printa
             printZahtev(zahtev);
+            System.out.println(output);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    static void printZahtev(Zahtev zahtev){
-        System.out.println(zahtev);
+    private static void printZahtev(Zahtev zahtev){
+        //info organa
+        
+        addToOutput("naziv organa", zahtev.getInfoOrgana().getNaziv());
+        addToOutput("sediste organa", zahtev.getInfoOrgana().getSediste());
+        addToOutput("naslov", zahtev.getNaslov());
+        addToOutput("svrha zahteva", zahtev.getSvrhaZahteva());
+
+        addToOutput("tekst zahteva", zahtev.getOpisZahteva().getTekstZahteva());
+        
+        
+    }
+
+
+
+    private static void addToOutput(String tag, String content){
+        String YELLOW = "\u001B[33m";
+        String BLUE = "\u001B[34m";
+        String PURPLE = "\u001B[35m";
+        output +=YELLOW + tag + ": " + BLUE + content + '\n';
     }
 
 }
