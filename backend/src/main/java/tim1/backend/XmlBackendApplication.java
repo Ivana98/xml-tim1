637da3a81e89b8ef7c0807fb450a4f9852a737d4
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-12-07 20:13:41 +0100

    Marshaling resenja uradjen

--- backend/src/main/java/tim1/backend/XmlBackendApplication.java
+++ backend/src/main/java/tim1/backend/XmlBackendApplication.java
@@ -4,11 +4,20 @@ package tim1.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import tim1.backend.model.resenje.MarshalingResenje;
+
 @SpringBootApplication
 public class XmlBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(XmlBackendApplication.class, args);
+
+		try {
+			MarshalingResenje.test();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 
 	}
 
--- backend/src/main/java/tim1/backend/model/resenje/MarshalingResenje.java
+++ backend/src/main/java/tim1/backend/model/resenje/MarshalingResenje.java
@@ -1,5 +1,77 @@
 package tim1.backend.model.resenje;
 
+import java.io.File;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.namespace.QName;
+
 public class MarshalingResenje {
+
+    public static void test() throws Exception {
+
+        try {
+            System.out.println("\n[INFO] Obavestenje JAXB Marshalling.\n");
+
+            JAXBContext context = JAXBContext.newInstance(ResenjeObrazac.class);
+
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            ResenjeObrazac resenjeObrazac = (ResenjeObrazac) unmarshaller.unmarshal(new File("./../documents/xml_documents/resenje.xml"));
+
+            resenjeObrazac.setDatum(getDatum());
+            resenjeObrazac.setPoverenik(getPoverenik());
+
+            JAXBElement<Double> jaxbCena = new JAXBElement(new QName("cena"), Double.class, 666.00);
+
+            resenjeObrazac.getSadrzaj().getObrazlozenje().getContent().set(1, jaxbCena);  //  postavi novu cenu
+
+            Marshaller marshaller = context.createMarshaller();
+
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+            marshaller.marshal(resenjeObrazac, System.out);
+
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Poverenik getPoverenik() {
+
+        Poverenik p = new Poverenik();
+
+        p.setIme("Dusan");
+        p.setPrezime("Dusanic");
+
+        return p;
+    }
     
+    private static XMLGregorianCalendar getDatum() {
+
+        Date date = new Date();
+
+        XMLGregorianCalendar xmlDate = null;
+        GregorianCalendar gc = new GregorianCalendar();
+        gc.setTime(date);
+
+        try {
+            xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // JAXBElement<XMLGregorianCalendar> jaxbDate = new JAXBElement(new QName("date"), XMLGregorianCalendar.class, xmlDate);
+
+        return xmlDate;
+    }
 }
