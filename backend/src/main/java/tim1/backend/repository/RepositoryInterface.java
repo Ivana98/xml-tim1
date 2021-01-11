3cb753fc196760efa853c67dc2c974a79e5cfd65
marijacurcic7 <52069615+marijacurcic7@users.noreply.github.com>
2021-01-11 21:28:44 +0100

    izmenjen repository

--- backend/src/main/java/tim1/backend/repository/RepositoryInterface.java
+++ backend/src/main/java/tim1/backend/repository/RepositoryInterface.java
@@ -1,15 +1,37 @@
 package tim1.backend.repository;
 
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.xmldb.api.modules.XMLResource;
 
-public interface RepositoryInterface {
+import tim1.backend.utils.DBManager;
+import tim1.backend.utils.FusekiManager;
+
+@Repository
+public abstract class RepositoryInterface {
+
+    @Autowired
+    private DBManager dbManager;
+  
+    @Autowired
+    private FusekiManager fusekiManager;
     
-    public XMLResource readXML(String id) throws Exception;
+    public XMLResource readXML(String id, String collectionId) throws Exception {
+        return dbManager.readFileFromDB(id, collectionId);
+    }
 
-    public void saveXML(String id, String content) throws Exception;
+    public void saveXML(String id, String content, String collectionId) throws Exception {
+        dbManager.saveFileToDB(id, collectionId, content);
+    }
 
-    public void readRDF(String uri)  throws Exception;
+    public void readRDF(String uri) throws Exception {
+        fusekiManager.readFile(uri);
+    }
 
-    public void saveRDF(String name, String uri)  throws Exception;
+    public void saveRDF(String name, String uri)  throws Exception {
+        fusekiManager.writeFuseki(name, uri);
+    }
 
 }
--- backend/src/main/java/tim1/backend/repository/ZalbaNaOdlukuRepository.java
+++ backend/src/main/java/tim1/backend/repository/ZalbaNaOdlukuRepository.java
@@ -10,43 +10,6 @@ import tim1.backend.utils.DBManager;
 import tim1.backend.utils.FusekiManager;
 
 @Repository
-public class ZalbaNaOdlukuRepository implements RepositoryInterface {
+public class ZalbaNaOdlukuRepository extends RepositoryInterface {
 
-  @Autowired
-  private DBManager dbManager;
-
-  @Autowired
-  private FusekiManager fusekiManager;
-
-  @Override
-  public XMLResource readXML(String id) throws Exception {
-    
-    return dbManager.readFileFromDB(id);
-  }
-
-  @Override
-  public void saveXML(String id, String content) throws Exception {
-
-    dbManager.saveFileToDB(id, content);
-  }
-
-  @Override
-  public void readRDF(String uri) {
-    try {
-      fusekiManager.readFile(uri);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-  }
-
-  @Override
-  public void saveRDF(String name, String uri) {
-    try {
-      fusekiManager.writeFuseki(name, uri);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-
-  }
 }
