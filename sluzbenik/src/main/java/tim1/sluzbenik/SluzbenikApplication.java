a4b330b475aa276f12229053e2b96d91de21f310
alekalek <alek@spectra-alarm.com>
2021-01-31 19:39:08 +0100

    dodata inicijalna bekend aplikacija za portal sluzbenika

---
+++ sluzbenik/src/main/java/tim1/sluzbenik/SluzbenikApplication.java
@@ -0,0 +1,13 @@
+package tim1.sluzbenik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SluzbenikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SluzbenikApplication.class, args);
+	}
+
+}
