f207d19dda17da3391d7b4c7fe5b3bb70813e3c3
alekalek <alek@spectra-alarm.com>
2021-02-03 22:13:24 +0100

    obrisane nepotrebne funkcije u zahtevController. Obrisan zahtev service i zahtev repository. Dodat ZahtevClient koji vrsi komunikaciju pomocu soapa sa sluzbenik aplikacijom

--- backend/src/main/java/tim1/backend/controller/ZahtevController.java
+++ backend/src/main/java/tim1/backend/controller/ZahtevController.java
@@ -1,85 +1,27 @@
 package tim1.backend.controller;
 
-import java.util.UUID;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.xmldb.api.base.XMLDBException;
-import org.xmldb.api.modules.XMLResource;
 
-import tim1.backend.model.liste.JaxbLista;
-import tim1.backend.model.zahtev.Zahtev;
-import tim1.backend.service.ZahtevService;
+import tim1.backend.soap.client.ZahtevClient;
 
 @RestController
 @RequestMapping(value = "/zahtevi")
 public class ZahtevController {
 
     @Autowired
-    private ZahtevService zahtevService;
+    private ZahtevClient zahtevClient;
 
-    @GetMapping(path = "/xml/{id}", produces = "application/xml")
-    public ResponseEntity<String> getXML(@PathVariable("id") String id) throws XMLDBException {
-
-        try {
-            XMLResource zahtev = zahtevService.readXML(id);
-            return new ResponseEntity<>(zahtev.getContent().toString(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @PostMapping(path = "/xml", consumes = "application/xml")
-    public ResponseEntity<?> saveXML(@RequestBody String content) {
-
-        String documentId = UUID.randomUUID().toString();
-
-        try {
-            zahtevService.saveXML(documentId, content);
-            zahtevService.saveRDF(content, documentId);
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping(path = "/rdf-xml/{uri}", produces = "application/xml")
-    public ResponseEntity<String> getRdfAsXML(@PathVariable("uri") String uri) {
-
-        try {
-            String rdf = zahtevService.readFileAsXML(uri);
-            return new ResponseEntity<>(rdf, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping(path = "/rdf-json/{uri}", produces = "application/json")
-    public ResponseEntity<String> getRdfAsJSON(@PathVariable("uri") String uri) {
-
-        try {
-            String rdf = zahtevService.readFileAsJSON(uri);
-            return new ResponseEntity<>(rdf, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
-    
     @GetMapping(path = "/xml", produces = "application/xml")
-    public ResponseEntity<JaxbLista<Zahtev>> findAllFromCollection() throws Exception{
+    public ResponseEntity<String> findAllFromCollection() throws Exception {
 
         try {
-            JaxbLista<Zahtev> lista = zahtevService.findAllFromCollection();
-            return new ResponseEntity<>(lista, HttpStatus.OK);
+            String listaZahteva = zahtevClient.findAll();
+            return new ResponseEntity<>(listaZahteva, HttpStatus.OK);
         } catch (Exception e) {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
--- backend/src/main/java/tim1/backend/soap/client/ZahtevClient.java
+++ backend/src/main/java/tim1/backend/soap/client/ZahtevClient.java
@@ -2,22 +2,17 @@ package tim1.backend.soap.client;
 
 import javax.xml.ws.Service;
 
-import tim1.backend.model.liste.JaxbLista;
-import tim1.backend.model.zahtev.Zahtev;
-
-import java.io.StringReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+
 import javax.xml.namespace.QName;
 
-// @Service
+@org.springframework.stereotype.Service
 public class ZahtevClient {
 
-  public void getOne() throws MalformedURLException, JAXBException {
+  public String getOne() throws MalformedURLException, JAXBException {
     URL wsdlLocation = new URL("http://localhost:8091/api/ws/zahtev?wsdl");
     QName serviceName = new QName("http://www.ftn.uns.ac.rs/zahtev", "ZahtevService");
     QName portName = new QName("http://www.ftn.uns.ac.rs/zahtev", "ZahtevServiceSoapBinding");
@@ -27,13 +22,10 @@ public class ZahtevClient {
     ZahtevServicePortType zServicePortType = service.getPort(portName, ZahtevServicePortType.class);
 
     String response = zServicePortType.getZahtevById("e9dda76a-48ad-4e74-9ff0-9d052f4ee4a0");
-    JAXBContext context = JAXBContext.newInstance(Zahtev.class);
-    Unmarshaller unmarshaller = context.createUnmarshaller();
-    StringReader reader = new StringReader(response);
-    Zahtev z = (Zahtev) unmarshaller.unmarshal(reader);
-    System.out.println(response);
-  } 
-  public void findAll() throws MalformedURLException, JAXBException {
+    return response;
+  }
+
+  public String findAll() throws MalformedURLException, JAXBException {
     URL wsdlLocation = new URL("http://localhost:8091/api/ws/zahtev?wsdl");
     QName serviceName = new QName("http://www.ftn.uns.ac.rs/zahtev", "ZahtevService");
     QName portName = new QName("http://www.ftn.uns.ac.rs/zahtev", "ZahtevServiceSoapBinding");
@@ -43,18 +35,13 @@ public class ZahtevClient {
     ZahtevServicePortType zServicePortType = service.getPort(portName, ZahtevServicePortType.class);
 
     String response = zServicePortType.getAll();
-    JAXBContext context = JAXBContext.newInstance(JaxbLista.class);
-    Unmarshaller unmarshaller = context.createUnmarshaller();
-    StringReader reader = new StringReader(response);
-    JaxbLista<Zahtev> lista = (JaxbLista<Zahtev>) unmarshaller.unmarshal(reader);
-    for (Zahtev  z : lista.getLista()) {
-      System.out.println(z.getNaslov());
-    }
-  } 
-
-  public static void main(String[] args) throws MalformedURLException, JAXBException {
-    ZahtevClient client = new ZahtevClient();
-    // client.getOne();
-    client.findAll();
+    return response;
   }
+
+  // public static void main(String[] args) throws MalformedURLException,
+  // JAXBException {
+  // ZahtevClient client = new ZahtevClient();
+  // // client.getOne();
+  // client.findAll();
+  // }
 }
