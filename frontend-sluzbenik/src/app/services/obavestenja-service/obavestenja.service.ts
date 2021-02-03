357b842f6c5c00018a5606886196246000b6cbc2
marijacurcic7 <52069615+marijacurcic7@users.noreply.github.com>
2021-02-03 11:56:23 +0100

    id zahteva se dodaje u obavestenje

--- frontend-sluzbenik/src/app/services/obavestenja-service/obavestenja.service.ts
+++ frontend-sluzbenik/src/app/services/obavestenja-service/obavestenja.service.ts
@@ -50,64 +50,6 @@ export class ObavestenjaService {
   public replaceArhivi = `<Dostavljeno Selected="2"><Primer ID="1">1. Именованом </Primer><Primer ID="2">2. Архиви</Primer></Dostavljeno>`;
   public replaceImenovanom = `<Dostavljeno Selected="1"><Primer ID="1">1. Именованом </Primer><Primer ID="2">2. Архиви</Primer></Dostavljeno>`;
 
-  public xmlString =
-    `<Obavestenje xmlns="http://www.ftn.uns.ac.rs/obavestenjecir" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://www.ftn.uns.ac.rs/obavestenjecir ../xsd_documents/obavestenjecir.xsd"
-  xmlns:pred="http://www.ftn.uns.ac.rs/rdf/examples/predicate/">
-      <Osnovni_podaci>
-        <Organ about="http://www.ftn.uns.ac.rs/rdf/obavestenje/Organ">
-          <Naziv property="pred:naziv" datatype="xs:string"> </Naziv>
-          <Sediste property="pred:sediste" datatype="xs:string"> </Sediste>
-        </Organ>
-        <Broj_predmeta> </Broj_predmeta>
-        <Datum property="pred:datum" datatype="xs:date"> </Datum>
-        <Podnosioc_zahteva about="http://www.ftn.uns.ac.rs/rdf/obavestenje/Podnosioc">
-            <Ime property="pred:ime" datatype="xs:string"> </Ime>
-            <Prezime property="pred:prezime" datatype="xs:string"> </Prezime>
-            <Naziv> </Naziv>
-            <Adresa property="pred:adresa" datatype="xs:string"> </Adresa>
-        </Podnosioc_zahteva>
-      </Osnovni_podaci>
-      <Sadrzaj>
-        <Naslov>O B A V E S T E N J E</Naslov>
-        <Podnaslov>о стављању на увид документа који садржи
-            тражену информацију и о изради копије
-        </Podnaslov>
-        <Paragraf>
-            На основу члана 16. ст. 1. Закона о слободном приступу информацијама од јавног
-            значаја, поступајући по вашем захтеву за слободан приступ информацијама од
-            <Godina> </Godina>
-            год., којим сте тражили увид у документ/е са информацијама о / у вези са:
-            <Opis_informacije>
-            </Opis_informacije>
-            обавештавамо вас да дана
-            <Datum_uvida> </Datum_uvida>, u
-            <Vreme_uvida> </Vreme_uvida> часова, односно у времену од
-            <Pocetak_cas> </Pocetak_cas> do
-            <Kraj_cas> </Kraj_cas> часова, у просторијама органа у
-            <Grad_organa> </Grad_organa> ул.
-            <Ulica_organa> </Ulica_organa> бр.
-            <Broj_organa> </Broj_organa>, канцеларија бр.
-            <Broj_kancelarije> </Broj_kancelarije> можете извршити увид у документ/е у коме је садржана
-            тражена информација.
-
-            Том приликом, на ваш захтев, може вам се издати и копија документа са траженом информацијом.
-
-            Трошкови су утврђени Уредбом Владе Републике Србије („Сл. гласник РС“, бр. 8/06), и то:
-            копија стране А4 формата износи 3 динара, А3 формата 6 динара, CD 35 динара, дискете 20
-            динара, DVD 40 динара, аудио-касета – 150 динара, видео-касета 300 динара, претварање
-            једне стране документа из физичког у електронски облик – 30 динара.
-
-            Износ укупних трошкова израде копије документа по вашем захтеву износи
-            <Iznos_troskova> </Iznos_troskova>
-            динара и уплаћује се на жиро-рачун Буџета Републике Србије бр. 840-742328-843-30, с
-            позивом на број 97 – ознака шифре општине/града где се налази орган власти (из
-            Правилника о условима и начину вођења рачуна – „Сл. гласник РС“, 20/07... 40/10).
-        </Paragraf>
-        <Dostavljeno> </Dostavljeno>
-      </Sadrzaj>
-  </Obavestenje>`;
-
   public obavestenjeSpecifikacija = {
     elements: {
       Obavestenje: {
@@ -237,6 +179,65 @@ export class ObavestenjaService {
         oneliner: true
       }
     }
+  }
+
+  public getXmlString(idZahteva: string) : string{
+    return  `<Obavestenje xmlns="http://www.ftn.uns.ac.rs/obavestenjecir" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.ftn.uns.ac.rs/obavestenjecir ../xsd_documents/obavestenjecir.xsd"
+    xmlns:pred="http://www.ftn.uns.ac.rs/rdf/examples/predicate/" id="" idZahteva="${idZahteva}">
+        <Osnovni_podaci>
+          <Organ about="http://www.ftn.uns.ac.rs/rdf/obavestenje/Organ">
+            <Naziv property="pred:naziv" datatype="xs:string"> </Naziv>
+            <Sediste property="pred:sediste" datatype="xs:string"> </Sediste>
+          </Organ>
+          <Broj_predmeta> </Broj_predmeta>
+          <Datum property="pred:datum" datatype="xs:date"> </Datum>
+          <Podnosioc_zahteva about="http://www.ftn.uns.ac.rs/rdf/obavestenje/Podnosioc">
+              <Ime property="pred:ime" datatype="xs:string"> </Ime>
+              <Prezime property="pred:prezime" datatype="xs:string"> </Prezime>
+              <Naziv> </Naziv>
+              <Adresa property="pred:adresa" datatype="xs:string"> </Adresa>
+          </Podnosioc_zahteva>
+        </Osnovni_podaci>
+        <Sadrzaj>
+          <Naslov>O B A V E S T E N J E</Naslov>
+          <Podnaslov>о стављању на увид документа који садржи
+              тражену информацију и о изради копије
+          </Podnaslov>
+          <Paragraf>
+              На основу члана 16. ст. 1. Закона о слободном приступу информацијама од јавног
+              значаја, поступајући по вашем захтеву за слободан приступ информацијама од
+              <Godina> </Godina>
+              год., којим сте тражили увид у документ/е са информацијама о / у вези са:
+              <Opis_informacije>
+              </Opis_informacije>
+              обавештавамо вас да дана
+              <Datum_uvida> </Datum_uvida>, u
+              <Vreme_uvida> </Vreme_uvida> часова, односно у времену од
+              <Pocetak_cas> </Pocetak_cas> do
+              <Kraj_cas> </Kraj_cas> часова, у просторијама органа у
+              <Grad_organa> </Grad_organa> ул.
+              <Ulica_organa> </Ulica_organa> бр.
+              <Broj_organa> </Broj_organa>, канцеларија бр.
+              <Broj_kancelarije> </Broj_kancelarije> можете извршити увид у документ/е у коме је садржана
+              тражена информација.
+  
+              Том приликом, на ваш захтев, може вам се издати и копија документа са траженом информацијом.
+  
+              Трошкови су утврђени Уредбом Владе Републике Србије („Сл. гласник РС“, бр. 8/06), и то:
+              копија стране А4 формата износи 3 динара, А3 формата 6 динара, CD 35 динара, дискете 20
+              динара, DVD 40 динара, аудио-касета – 150 динара, видео-касета 300 динара, претварање
+              једне стране документа из физичког у електронски облик – 30 динара.
+  
+              Износ укупних трошкова израде копије документа по вашем захтеву износи
+              <Iznos_troskova> </Iznos_troskova>
+              динара и уплаћује се на жиро-рачун Буџета Републике Србије бр. 840-742328-843-30, с
+              позивом на број 97 – ознака шифре општине/града где се налази орган власти (из
+              Правилника о условима и начину вођења рачуна – „Сл. гласник РС“, 20/07... 40/10).
+          </Paragraf>
+          <Dostavljeno> </Dostavljeno>
+        </Sadrzaj>
+    </Obavestenje>`;
   }
 
 }
