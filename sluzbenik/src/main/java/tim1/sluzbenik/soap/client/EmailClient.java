fe37e2a4bc08a062921f119c80d4f86e4ea94add
alekalek <alek@spectra-alarm.com>
2021-02-04 10:38:11 +0100

    dodat email soap

---
+++ sluzbenik/src/main/java/tim1/sluzbenik/soap/client/EmailClient.java
@@ -0,0 +1,10 @@
+package tim1.sluzbenik.soap.client;
+
+@org.springframework.stereotype.Service
+public class EmailClient {
+  
+  public String odbijZahtev() {
+    //TODO: NAPRAVITI KONEKCIJU SA MEJLOM.
+    return null;
+  }
+}
