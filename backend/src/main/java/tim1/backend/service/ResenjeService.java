b9a7e934e004334b7577a8e7eb9621dfac4a9478
Ivana Marosevic <i.marosevic@hotmail.com>
2021-02-04 22:56:48 +0100

    zavrsena konfiguracija backenda za transformacije

--- backend/src/main/java/tim1/backend/service/ResenjeService.java
+++ backend/src/main/java/tim1/backend/service/ResenjeService.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XMLResource;
 
 import tim1.backend.model.liste.JaxbLista;
 import tim1.backend.model.resenje.Resenje;
 import tim1.backend.repository.ResenjeRepository;
+import tim1.backend.utils.XSLFORTransformer;
+
+import static tim1.backend.utils.PathConstants.*;
 
 @Service
 public class ResenjeService extends AbstractService {
@@ -22,6 +27,33 @@ public class ResenjeService extends AbstractService {
         List<Resenje> listaZalbi = this.findAllFromCollection( Resenje.class);
         JaxbLista<Resenje> listaObj = new JaxbLista<Resenje>(listaZalbi);
         return listaObj;
+    }
+
+    public String generateHTML(String id) throws XMLDBException {
+		XSLFORTransformer transformer = null;
+
+		try {
+			transformer = new XSLFORTransformer();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+        XMLResource xmlRes = this.readXML(id);
+		String doc_str = xmlRes.getContent().toString();
+		boolean ok = false;
+		String html_path = SAVE_HTML + "resenje_" + id + ".html";
+
+		try {
+			ok = transformer.generateHTML(doc_str, html_path, RESENJE_XSL);
+			if (ok)
+				return html_path;
+			else
+				return null;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
     }
 
 }
