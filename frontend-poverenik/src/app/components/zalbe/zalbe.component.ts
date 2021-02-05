c0f926ff5cf109ac2bdc18a8ae9f83c30faf0b41
alekalek <alek@spectra-alarm.com>
2021-02-05 12:59:19 +0100

    sredjene zalbe na frontu u tabeli poverenika

--- frontend-poverenik/src/app/components/zalbe/zalbe.component.ts
+++ frontend-poverenik/src/app/components/zalbe/zalbe.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Console } from 'console';
 import { Zalba } from 'src/app/model/zalba';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { ZalbaService } from 'src/app/services/zalba/zalba.service';
@@ -11,7 +12,7 @@ import { ZalbaService } from 'src/app/services/zalba/zalba.service';
   styleUrls: ['./zalbe.component.scss']
 })
 export class ZalbeComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'naziv', 'korisnik', 'izvoz', 'resenje'];
+  displayedColumns: string[] = ['id', 'naziv', 'izvoz', 'resenje'];
   dataSource: MatTableDataSource<Zalba>;
   pageIndex: number = 0;
   pageSize: number = 5;
@@ -33,55 +34,60 @@ export class ZalbeComponent implements OnInit {
     this.getAll()
   }
 
-  getAll() {
+  async getAll() {
     this.zalbe = []; //praznimo listu svaki put kada je ponovo punimo
-    this.getAllCutanje();
+    await this.getAllCutanje();
 
     //TODO: ovo baca gresku neku. pogledati sam marijom
-    // this.getAllOdbijanje(); 
-  }
-
-  getAllCutanje() {
-    this.zalbeService.getAllCutanje()
-      .subscribe(
-        data => {
-          let lista = data["jaxbLista"]["ns3:Zalba_na_cutanje"];
-
-          console.log(lista);
-
-          this.popuniListu(lista);
-        }
-      );
-  }
-
-  getAllOdbijanje() {
-    this.zalbeService.getAllOdbijanje()
-      .subscribe(
-        data => {
-          let lista = data["jaxbLista"]["ns3:Zalba_na_cutanje"];
-
-          console.log(lista);
-
-          this.popuniListu(lista);
-        }
-      );
-  }
-
-  popuniListu(lista) {
-    lista.forEach(element => {
-      // gradjanin moze da vidi samo svoje zalbe
-      // dok poverenik moze da vidi sve zalbe
-      if (this.role == "GRADJANIN") {
-        if (element["ns3:Podnosilac_zalbe"]["$"]["email"] == this.email) {
-          this.zalbe.push(new Zalba(element["$"]["id"], "Zalba na cutanje", element["ns3:Podnosilac_zalbe"]["$"]["email"]))
-        }
-      } else {
-        this.zalbe.push(new Zalba(element["$"]["id"], "Zalba na cutanje", element["ns3:Podnosilac_zalbe"]["$"]["email"]))
-      }
-    });
+    await this.getAllOdbijanje();
 
     this.dataSource = new MatTableDataSource<Zalba>(this.zalbe);
+
+
   }
+
+  async getAllCutanje() {
+    let lista = await this.zalbeService.getAllCutanje().toPromise();
+    lista = lista["jaxbLista"]["ns3:Zalba_na_cutanje"];
+    console.log(lista);
+    this.dodajZalbeNaCutanje(lista);
+  }
+
+  async getAllOdbijanje() {
+    let lista = await this.zalbeService.getAllOdbijanje().toPromise();
+    lista = lista["jaxbLista"]["ns4:zalba_na_odluku"];
+    // console.log(lista);
+    this.dodajZalbeNaOdluku(lista);
+  }
+
+  dodajZalbeNaCutanje(lista: any[]) {
+    // gradjanin moze da vidi samo svoje zalbe
+    // dok poverenik moze da vidi sve zalbe
+    if (this.role == "KORISNIK") {
+      lista = lista.filter(zalba => zalba["ns3:Podnosilac_zalbe"]["$"]["email"] == this.email);
+    }
+
+    lista.forEach(element => {
+      this.zalbe.push(new Zalba(element["$"]["id"], "Zalba na cutanje", element["ns3:Podnosilac_zalbe"]["$"]["email"]))
+    });
+  }
+
+  dodajZalbeNaOdluku(lista: any[]) {
+    // gradjanin moze da vidi samo svoje zalbe
+    // dok poverenik moze da vidi sve zalbe
+    if (this.role == "KORISNIK") {
+      lista = lista.filter(zalba => zalba["ns4:zalba"]["ns4:podnosilac"]["$"]["email"] == this.email);
+    }
+
+    lista.forEach(element => {
+      let id = element["$"]["id"];
+      let korisnik = element["ns4:zalba"]["ns4:podnosilac"]["ns4:ime"]["_"] + ' ' + element["ns4:zalba"]["ns4:podnosilac"]["ns4:prezime"]["_"];
+      this.zalbe.push(new Zalba(id, "Zalba na odluku", korisnik))
+    });
+
+  }
+
+
 
   requestPage(): void {
     let event = new PageEvent();
