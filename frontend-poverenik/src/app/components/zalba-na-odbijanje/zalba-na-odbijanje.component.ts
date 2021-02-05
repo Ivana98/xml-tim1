99ec1ed453ee5ea9e6779ed93fff5ad376446fe3
marijacurcic7 <52069615+marijacurcic7@users.noreply.github.com>
2021-02-05 02:49:19 +0100

    ispravljeno dodavanje zalbe na odluku

--- frontend-poverenik/src/app/components/zalba-na-odbijanje/zalba-na-odbijanje.component.ts
+++ frontend-poverenik/src/app/components/zalba-na-odbijanje/zalba-na-odbijanje.component.ts
@@ -44,37 +44,37 @@ export class ZalbaNaOdbijanjeComponent implements OnInit {
   }
 
   send() {
-    var zalba = `<?xml version="1.0" encoding="UTF-8"?>
+    let zalba = `<?xml version="1.0" encoding="UTF-8"?>
     <zalba_na_odluku
         xmlns="http://www.ftn.uns.ac.rs/zalba-na-odluku"
         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:schemaLocation="http://www.ftn.uns.ac.rs/zalba-na-odluku ../xsd_documents/zalbanaodlukucir.xsd"
         xmlns:pred="http://www.ftn.uns.ac.rs/rdf/examples/predicate/"
         id="" idZahteva="${this.zahtevId}">
-        <naslov> ЖАЛБА  ПРОТИВ  ОДЛУКЕ 2 </naslov>
+        <naslov> ЖАЛБА  ПРОТИВ  ОДЛУКЕ ОРГАНА  ВЛАСТИ КОЈОМ ЈЕ ОДБИЈЕН ИЛИ ОДБАЧЕН ЗАХТЕВ ЗА ПРИСТУП ИНФОРМАЦИЈИ </naslov>
         
         <podaci_o_povereniku>
             <namena> 
                 Поверенику за информације од јавног значаја и заштиту података о личности 
             </namena>
-            <adresa>
-                <grad property="pred:grad" datatype="xs:string">${this.grad}</grad>
-                <ulica property="pred:ulica" datatype="xs:string">${this.ulica}</ulica>
+            <adresa about="http://www.ftn.uns.ac.rs/rdf/zalbaodluka/AdresaPoverenika">
+                <grad property="pred:grad" datatype="xs:string"> ${this.grad} </grad>
+                <ulica property="pred:ulica" datatype="xs:string"> ${this.ulica} </ulica>
                 <broj>${this.broj}</broj>
             </adresa>
         </podaci_o_povereniku>
         
         <zalba>
             Ж А Л Б А  
-            <zalilac type = "TFizickoLice">
+            <zalilac xsi:type="TFizickoLice" about="http://www.ftn.uns.ac.rs/rdf/zalbaodluka/Zalilac">
                 <adresa about="http://www.ftn.uns.ac.rs/rdf/zalbaodluka/AdresaZalioca">
-                    <grad>${this.grad}</grad>
-                    <ulica>${this.ulica}</ulica>
+                    <grad property="pred:grad" datatype="xs:string">${this.grad}</grad>
+                    <ulica property="pred:grad" datatype="xs:string">${this.ulica}</ulica>
                     <broj>${this.broj}</broj>
                 </adresa>
-                <sediste>${this.sediste}</sediste>
-                <ime>${this.ime}</ime>
-                <prezime>${this.prezime}</prezime>
+                <sediste property="pred:sediste" datatype="xs:string">${this.sediste}</sediste>
+                <ime property="pred:ime" datatype="xs:string">${this.ime}</ime>
+                <prezime property="pred:prezime" datatype="xs:string">${this.prezime}</prezime>
             </zalilac>
             против решења-закључка 
             
@@ -82,8 +82,11 @@ export class ZalbaNaOdbijanjeComponent implements OnInit {
             Број <broj_zahteva>${this.brojZahteva}</broj_zahteva> од <godina>${this.godina}</godina> године.
             
             <sadrzaj>
-                Sadrzaj zalbe na odluku <datum>${this.datumPodnosenjaZahteva}</datum> Oдлуку побијам у
-                целости, односно у делу којим <deo_odluke>${this.deoOdluke}</deo_odluke> јер није заснована на Закону о слободном приступу информацијама од јавног значаја.
+                Наведеном одлуком органа власти (решењем, закључком, обавештењем у писаној форми 
+                са елементима одлуке) , супротно закону, одбијен-одбачен је мој захтев који сам поднео/ла-
+                упутио/ла дана <datum>${this.datumPodnosenjaZahteva}</datum> године и тако ми ускраћено-онемогућено остваривање уставног и
+                законског права на слободан приступ информацијама од јавног значаја. Oдлуку побијам у
+                целости, односно у делу којим <deo_odluke>${this.deoOdluke} </deo_odluke> јер није заснована на Закону о слободном приступу информацијама од јавног значаја.
                 На основу изнетих разлога, предлажем да Повереник уважи моју жалбу,  поништи
                 одлука првостепеног органа и омогући ми приступ траженој/им  информацији/ма.
                 Жалбу подносим благовремено, у законском року утврђеном у <zakon>члану 22. ст. 1. Закона о слободном приступу информацијама од јавног значаја.</zakon>   
@@ -101,8 +104,8 @@ export class ZalbaNaOdbijanjeComponent implements OnInit {
             </podnosilac>
             
             <vreme_i_mesto about="http://www.ftn.uns.ac.rs/rdf/zalbaodluka/Podnosenje">
-                <grad property="pred:mestoPodnosenja" datatype="xs:string">${this.mestoPodnosenjaZalbe}</grad>
-                <datum property="pred:datumPodnosenja" datatype="xs:date">${this.datumPodnosenjaZalbe}</datum>
+                <grad property="pred:mesto" datatype="xs:string">${this.mestoPodnosenjaZalbe}</grad>
+                <datum property="pred:datum" datatype="xs:date">${this.datumPodnosenjaZalbe}</datum>
             </vreme_i_mesto>
             
         </zalba>
@@ -110,15 +113,19 @@ export class ZalbaNaOdbijanjeComponent implements OnInit {
         <napomene>
             Напомена: 
             <napomena id="1">
-                Napomena 1
+                У жалби се мора навести одлука која се побија (решење, закључак, обавештење), назив
+                органа који је одлуку донео, као и број и датум одлуке. Довољно је да жалилац наведе у
+                жалби у ком погледу је незадовољан одлуком, с тим да жалбу не мора посебно образложити.
+                Ако жалбу изјављује на овом обрасцу, додатно образложење може  посебно приложити. 
             </napomena>
             <napomena id="2">
-                Napomena 2
+                Уз жалбу обавезно приложити копију поднетог захтева и доказ о његовој предаји-упућивању органу
+                као и копију одлуке органа која се оспорава жалбом.
             </napomena>
         </napomene>
         
-    </zalba_na_odluku>`
-
+    </zalba_na_odluku>
+    `
     this.zalbaService.addNewOdbijanje(zalba)
     .subscribe(
       data => {
